0ecae00cea8e97d6441b1f9b55dfdba3cad5bfd2
peacevan\amado <peacevan@gmail.com>
2021-06-05 12:11:12 -0300

    correções

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -10,20 +10,15 @@ class Controller {
      * @return mixed
      * 
      */
-    
-    
-    
+   
     public function view($view, $data = []) {
         $file = "../src/views/{$view}.php";
-       
-        
         if (file_exists($file)) {
             require_once $file;
         } else {
             throw new Exception("Error Processing Request", 1);
         }
     }
-
     /**
      * @return  mixed
      * 
